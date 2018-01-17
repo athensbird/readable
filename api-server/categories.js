6b2e609f94582ddc41f33c9db423c4e05b14677b
athensbird <popularwatson@gmail.com>
2018-01-17 15:43:17 -0600

    server re-rendered

--- api-server/categories.js
+++ api-server/categories.js
@@ -16,6 +16,14 @@ const defaultData = {
       {
         name: 'udacity',
         path: 'udacity'
+      },
+      {
+        name: 'food',
+        path: 'food'
+      },
+      {
+        name: 'baking',
+        path: 'baking'
       }
   ]
 }
